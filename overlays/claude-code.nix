a4b83cde1ec92c628009bb8811f2fc19b24c2eed
tim-oster <tim.oster99@gmail.com>
2025-11-30 17:43:25 +0100

    update: claude-code

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,10 +1,10 @@
 final: prev: {
   claude-code = prev.claude-code.overrideAttrs (oldAttrs: rec {
-    version = "1.0.88";
+    version = "2.0.55";
 
     src = prev.fetchzip {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-      hash = "sha256-o0A9P0sBB2Fk18CArGGv/QBi55ZtFgoJ2/3gHlDwyEU=";
+      hash = "sha256-wsjOkNxuBLMYprjaZQyUZHiqWl8UG7cZ1njkyKZpRYg=";
     };
 
     npmDepsHash = "";
